70e846896a36a48ea7eccf55c1023005851e1341
syeadmaaz <syeadmaazahmed@gmail.com>
2022-06-24 21:51:41 +0530

    update

---
+++ models/userMaster.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+mongoose.pluralize(null);
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    address:{
+        type: String,
+        default: null,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
