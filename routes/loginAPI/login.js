068617aa62ea071424ed4c2319feeed57395dd16
syeadmaaz <syeadmaazahmed@gmail.com>
2022-07-03 00:29:13 +0530

    updated login

--- routes/loginAPI/login.js
+++ routes/loginAPI/login.js
@@ -1,7 +1,7 @@
-const User = require("../../model/userMaster")
-const validateFunction = require("../../utility/validateFunction")
-const crypto = require("crypto")
-const crypt = require("../../utility/crypt")
+const User = require("../../model/userMaster");
+const validateFunction = require("../../utility/validateFunction");
+const crypto = require("crypto");
+const crypt = require("../../utility/crypt");
 
 const secret_key = process.env.SECRET_KEY;
 const secret_iv = process.env.SECRET_IV;
@@ -17,62 +17,61 @@ const iv = crypto
   .digest("hex")
   .substr(0, 16);
 
+exports.login = async (req, res) => {
+  const { userName, password } = req.query;
 
-exports.login = async (req, res) =>{
-    const {userName,password} = req.query;
+  console.log(req.query);
 
-    console.log(req.query);
-
-    try{
-        if (!userName || !password) {
-            return res.status(422).json({ error: "Please fill the fields properly" });
-        }
-
-        if(validateFunction.validateEmail(userName)) {
-
-            email=userName;
-
-            userExist  = await User.findOne({email})
-            console.log(userExist);
-
-            if(!userExist) return res.status(422).json({ error: "Please SignUp First" });
-
-            let decpassword = crypt.decrypt(userExist.password, secretKey, iv);
-
-            console.log(decpassword);
-
-            if(decpassword===password) return res.status(201).json({ message: "SignedIN Successfully"});
-
-            if(decpassword!==password)
-            return res.status(422).json({ error: "Please fill the Password correctly" });
-
-        }
-
-        else if(validateFunction.validateMobileNo(userName)){
-
-            mobile=userName;
-
-            userExist  = await User.findOne({mobile})
-            // console.log(userExist);
-
-            if(!userExist) return res.status(422).json({ error: "Please SignUp First" });
-        
-            let decpassword = crypt.decrypt(userExist.password, secretKey, iv);
-
-            // console.log(decpassword);
-
-            if(decpassword===password) return res.status(201).json({ message: "SignedIN Successfully"});
-
-            if(decpassword!==password)
-            return res.status(422).json({ error: "Please fill the Password correctly" });
-        }
-
-        else
-        return res.status(422).json({ error: "Please fill the Email or Mobile Correctly" });
-
+  try {
+    if (!userName || !password) {
+      return res.status(422).json({ error: "Please fill the fields properly" });
     }
 
-    catch(err){
-        console.log(err)
-    }
-}
+    if (validateFunction.validateEmail(userName)) {
+      email = userName;
+
+      userExist = await User.findOne({ email });
+      console.log(userExist);
+
+      if (!userExist)
+        return res.status(422).json({ error: "Incorrect Email ID/Password" });
+
+      let decpassword = crypt.decrypt(userExist.password, secretKey, iv);
+
+      console.log(decpassword);
+
+      if (decpassword === password)
+        return res.status(201).json({ message: "SignIn Successful" });
+      // if(decpassword!==password)
+      else
+        return res
+          .status(422)
+          .json({ error: "Fill your credentials properly" });
+    } else if (validateFunction.validateMobileNo(userName)) {
+      mobile = userName;
+
+      userExist = await User.findOne({ mobile });
+      // console.log(userExist);
+
+      if (!userExist)
+        return res.status(422).json({ error: "Incorrect Mobile No/Password" });
+
+      let decpassword = crypt.decrypt(userExist.password, secretKey, iv);
+
+      // console.log(decpassword);
+
+      if (decpassword === password)
+        return res.status(201).json({ message: "SignIn Successful" });
+
+      if (decpassword !== password)
+        return res
+          .status(422)
+          .json({ error: "Fill your credentials properly"});
+    } else
+      return res
+        .status(422)
+        .json({ error: "Please fill your credentials properly"});
+  } catch (err) {
+    console.log(err);
+  }
+};
